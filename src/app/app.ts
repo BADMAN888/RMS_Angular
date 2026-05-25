7b730993531c23f7ead91c6c34ca8b7ca4750815
Vlad <vlad20020502@gmail.com>
2026-05-25 13:20:54 +0300

    v2

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,14 +1,12 @@
-import { Component } from '@angular/core'
-import { RouterOutlet } from '@angular/router'
+import { Component, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [
-    RouterOutlet
-  ],
-  templateUrl: './app.component.html'
+  imports: [RouterOutlet],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
 })
-export class AppComponent {
-
+export class App {
+  protected readonly title = signal('RMS');
 }
