6fab675ca0bcba010069155e0826a27d052be955
Vlad <vlad20020502@gmail.com>
2026-05-25 13:20:54 +0300

    v3

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,16 +1,15 @@
 import { Routes } from '@angular/router'
 import { PublicLayoutComponent } from './layouts/public-layout/public-layout.component'
 import { HomePageComponent } from './features/home/pages/home-page/home-page.component'
+import { MenuPageComponent } from './features/dishes/pages/menu-page/menu-page.component'
 
 export const routes: Routes = [
   {
     path: '',
     component: PublicLayoutComponent,
     children: [
-      {
-        path: '',
-        component: HomePageComponent
-      }
+      { path: '', component: HomePageComponent },
+      { path: 'menu', component: MenuPageComponent }
     ]
   }
 ]
