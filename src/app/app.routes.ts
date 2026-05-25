a9647e2274f94b39fc30aa6b2bbbfeba7839dd1f
Vlad <vlad20020502@gmail.com>
2026-05-25 13:17:05 +0300

    v1

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
-import { Routes } from '@angular/router';
+import { Routes } from '@angular/router'
+import { PublicLayoutComponent } from './layouts/public-layout/public-layout.component'
+import { HomePageComponent } from './features/home/pages/home-page/home-page.component'
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: PublicLayoutComponent,
+    children: [
+      {
+        path: '',
+        component: HomePageComponent
+      }
+    ]
+  }
+]
